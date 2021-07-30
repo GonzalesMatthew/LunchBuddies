7327b6b43e9cc5a4074a04248c6df714039e0efc
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-07-30 18:29:57 -0500

    Add project files.

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBuddies
+{
+    class LunchBuddy
+    {
+        public string FirstName { get; set; }
+        public string LastName {get; set; }
+
+        public void Eat()
+        {
+
+        }
+    }
+}
---
+++ LunchBuddies/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBuddies
+{
+    class Restaurant
+    {
+        private List<string> RestaurantNames { get; set; }
+        public string Name { get; set; }
+
+        if(RestaurantNames == null) 
+        {
+            RestaurantNames = new List<string> {"Pink Flamingo", "The Rusty Bucket", "The Lady's Hankerchief"};
+        }   
+        int index = Random.Next(RestaurantNames.Count);
+        
+    }
+}
